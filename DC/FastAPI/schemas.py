a3f5f3bec92f84a836ff74dfa3ed9b18d582fa90
Washington Bispo <juniorbispo515@gmail.com>
2021-09-27 12:57:38 -0300

    Removendo duplicata
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>

--- DC/FastAPI/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Matriz(BaseModel):
-	matrizA: List[List[float]] = [[]]
-	matrizB: List[List[float]] = [[]]
-
-class Numeros(BaseModel):
-	numeros: List[int] = []
