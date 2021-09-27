9aca1bfff06b01cbd810d47d2441f77a74909181
Washington Bispo <juniorbispo515@gmail.com>
2021-09-26 21:07:53 -0300

    Adicionando método de Strassen
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>

--- FastAPI/Calculos/repository/Calculos.py
+++ FastAPI/Calculos/repository/Calculos.py
@@ -13,9 +13,10 @@ def karatsuba(request: schemas.Numeros):
 	return Multiplicar(bin(request.numeros[0])[2:],bin(request.numeros[1])[2:])
 
 def Strassen(request: schemas.Matriz):
-	return request
+	return {"Response": strassen(np.array(request.matrizA), np.array(request.matrizB)).tolist()}  
 
 
+# Karatsuba 
 def Igualar(str1, str2):
 	if(len(str1) < len(str2)):
 		str1 = "0" *  (len(str2) - len(str1)) + str1
@@ -58,10 +59,47 @@ def Multiplicar(str1, str2):
 	Yl = str2[0:fh]
 	Yr = str2[fh:]
 
-  # XR e Yr tรก bugado
 
 	P1 = Multiplicar(Xl, Yl);
 	P2 = Multiplicar(Xr, Yr);
 	P3 = Multiplicar(addBitStrings(Xl, Xr), addBitStrings(Yl, Yr));
 
 	return P1*(1<<(2*sh)) + (P3 - P1 - P2)*(1<<sh) + P2
+	
+def separa(matrix):
+    """
+    Splits a given matrix into quarters.
+    Input: nxn matrix
+    Output: tuple containing 4 n/2 x n/2 matrices corresponding to a, b, c, d
+    """
+    linha, coluna = matrix.shape
+    linha2, coluna2 = linha//2, coluna//2
+    return matrix[:linha2, :coluna2], matrix[:linha2, coluna2:], matrix[linha2:, :coluna2], matrix[linha2:, coluna2:]
+ 
+def strassen(m1, m2):
+ 
+    # Caso Base ou seja nossa matriz é uma 1x1
+    if len(m1) == 1:
+        return m1 * m2
+ 
+    # Separa a matriz em quatro com uma cruz no meio
+    a, b, c, d = separa(m1)
+    e, f, g, h = separa(m2)
+ 
+    # Calcula os produtos
+    p1 = strassen(a, f - h) 
+    p2 = strassen(a + b, h)       
+    p3 = strassen(c + d, e)       
+    p4 = strassen(d, g - e)       
+    p5 = strassen(a + d, e + h)       
+    p6 = strassen(b - d, g + h) 
+    p7 = strassen(a - c, e + f) 
+ 
+    c11 = p5 + p4 - p2 + p6 
+    c12 = p1 + p2          
+    c21 = p3 + p4           
+    c22 = p1 + p5 - p3 - p7 
+ 
+    c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+
+    return c
